006f179cd124cb56dbfeb39b31444838431e8165
AnxysUaen <Anxys@outlook.com>
2023-11-24 21:59:54 +0800

    能用了！

---
+++ README.md
@@ -0,0 +1,5 @@
+# ReloadSteamIcon
+
+修复重装 Steam 后,再次创建桌面快捷方式图标空白的问题
+
+此项目依赖 [steamcmd](https://www.steamcmd.net/) 项目
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,12 +12,32 @@ import (
 )
 
 var (
-	steamClientIconBaseURL  = "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/"
 	steamPossibleFolderList = []string{"Program Files (x86)/Steam", "Program Files/Steam", "Steam"}
 	steamInstalledFolder    string
 	steamIconFolder         string
 	installedGameIDList     []string
 )
+
+type GameInfo struct {
+	Data map[string]AppIDInfo `json:"data"`
+}
+
+type AppIDInfo struct {
+	Common struct {
+		Clienticon    string `json:"clienticon"`
+		Name          string `json:"name"`
+		NameLocalized struct {
+			Schinese string `json:"schinese"`
+		} `json:"name_localized"`
+		Type string `json:"type"`
+	} `json:"common"`
+}
+
+type NeedData struct {
+	icon  string
+	name  string
+	cname string
+}
 
 func getDiskList() []string {
 	diskList := []string{}
@@ -31,17 +52,44 @@ func getDiskList() []string {
 	return diskList
 }
 
-func getGameInfo(appId string) string {
-	url := "http://api.steamcmd.net/v1/info/" + appId
+func getGameInfo(appId string) NeedData {
+	url := fmt.Sprintf("http://api.steamcmd.net/v1/info/%s", appId)
 	req, _ := http.NewRequest("GET", url, nil)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil || res.StatusCode != 200 {
-		return ""
+		fmt.Print("请求失败，正在重试")
+		getGameInfo(appId)
+	}
+	defer res.Body.Close()
+	var gameInfo GameInfo
+	body, _ := io.ReadAll(res.Body)
+	json.Unmarshal(body, &gameInfo)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var resData NeedData
+	for _, appInfo := range gameInfo.Data {
+		resData = NeedData{
+			icon:  appInfo.Common.Clienticon,
+			name:  appInfo.Common.Name,
+			cname: appInfo.Common.NameLocalized.Schinese,
+		}
+	}
+	return resData
+}
+
+func getIconFile(appId string, icon string) []byte {
+	// http://cdn.cloudflare.steamstatic.com/
+	iconReqUrl := fmt.Sprintf("http://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/%s/%s.ico", appId, icon)
+	req, _ := http.NewRequest("GET", iconReqUrl, nil)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil || res.StatusCode != 200 {
+		fmt.Print("下载失败，正在重试")
+		getIconFile(appId, icon)
 	}
 	defer res.Body.Close()
 	body, _ := io.ReadAll(res.Body)
-	fmt.Println(string(body))
-	return string(body)
+	return body
 }
 
 func scanSteamInstallation() {
@@ -83,49 +131,57 @@ func scanSteamGameId() {
 	} else {
 		if file, err := os.Open(libraryVdf); err != nil {
 			fmt.Printf("ERROR! [%s]读取失败", libraryVdf)
-			return
 		} else {
 			defer file.Close()
-			if vdfMapFile, err := vdf.NewParser(file).Parse(); err != nil {
+			if vdfMap, err := vdf.NewParser(file).Parse(); err != nil {
 				fmt.Printf("ERROR! 解析配置文件错误")
+				return
 			} else {
-				if vdfMap, ok := vdfMapFile["Console Sample v.1"].(map[string]interface{}); ok {
-					if libMap, ok := vdfMap["libraryfolders"].(map[string]interface{}); ok {
-						for _, curLibrary := range libMap {
-							curLibIDList := []string{}
-							if curLibraryMap, ok := curLibrary.(map[string]interface{}); ok {
-								if appIdMap, ok := curLibraryMap["apps"].(map[string]interface{}); ok {
-									for appKey, appValue := range appIdMap {
-										fmt.Println(appKey)
-										curLibIDList = append(curLibIDList, fmt.Sprintf("%v", appValue))
-									}
-									installedGameIDList = append(installedGameIDList, curLibIDList...)
+				if libMap, ok := vdfMap["libraryfolders"].(map[string]interface{}); ok {
+					for _, curLibrary := range libMap {
+						curLibIDList := []string{}
+						if curLibraryMap, ok := curLibrary.(map[string]interface{}); ok {
+							if appIdMap, ok := curLibraryMap["apps"].(map[string]interface{}); ok {
+								for appKey := range appIdMap {
+									curLibIDList = append(curLibIDList, fmt.Sprintf("%v", appKey))
 								}
+								installedGameIDList = append(installedGameIDList, curLibIDList...)
 							}
 						}
 					}
 				}
-				fmt.Println(installedGameIDList)
-				return
 			}
 		}
 	}
 }
 
 func reloadSteamIcon() {
-	for _, app_id := range installedGameIDList {
-		// 请求接口获得信息
-		getGameInfo(app_id)
-		// 获取name用来显示
-		// 获取clienticon下载图标
-		// http://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/{{appid}}/{{icon}}.ico
-		// 检查是否下载成功
-		// 检查本地是否存在
-		// 替换
-		// 替换是否成功
-		// 添加延迟以免被ban
+	for _, appId := range installedGameIDList {
+		info := getGameInfo(appId)
+		displayName := info.name
+		if info.cname != "" {
+			displayName = info.cname
+		}
+		if info.icon != "" {
+			if _, err := os.Stat(path.Join(steamIconFolder, info.icon, ".ico")); err != nil {
+				fmt.Printf("正在下载 %s 的图标文件... ", displayName)
+				iconFile := getIconFile(appId, info.icon)
+				file, err := os.Create(path.Join(steamIconFolder, info.icon+".ico"))
+				if err != nil {
+					fmt.Printf("创建文件错误\n")
+				} else {
+					defer file.Close()
+					file.Write(iconFile)
+					fmt.Printf("下载完成\n")
+				}
+			} else {
+				fmt.Printf("存在 %s 的图标文件，跳过\n", displayName)
+			}
+		} else {
+			fmt.Printf("%s 不存在图标\n", displayName)
+		}
 	}
-	return
+	fmt.Println("修复完成")
 }
 
 func main() {
